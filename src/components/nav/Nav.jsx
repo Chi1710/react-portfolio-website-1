2628369037e040b6b64d922b5245a65b26cf8612
Chi <chiyantsestudio@gmail.com>
2023-01-20 21:45:42 -0500

    added link icon to selected components.add id to each components' section tag to correspones to link icon.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "./nav.css";
+import {FaHome} from 'react-icons/fa'
+import {FaRegUserCircle} from "react-icons/fa"
+import {RiBook3Line} from 'react-icons/ri'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageDetail} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href=''><FaHome /></a>
+      <a href='#about'><FaRegUserCircle /></a>
+       <a href='#experience'><RiBook3Line /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><BiMessageDetail /></a>
+
+    </nav>
   )
 }
 
