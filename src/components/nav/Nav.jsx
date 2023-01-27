c09db936d32565352dc934f1f55a9a4ba43c22b0
Chi <chiyantsestudio@gmail.com>
2023-01-26 21:51:08 -0500

    Style the nav bar to be transparent, flex on the bottom center. nav icon background light up when user is on the corresponding page se ction

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,33 @@ import {FaRegUserCircle} from "react-icons/fa"
 import {RiBook3Line} from 'react-icons/ri'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageDetail} from 'react-icons/bi'
+import { useState } from 'react';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href=''><FaHome /></a>
-      <a href='#about'><FaRegUserCircle /></a>
-       <a href='#experience'><RiBook3Line /></a>
-      <a href='#services'><RiServiceLine /></a>
-      <a href='#contact'><BiMessageDetail /></a>
+      <a href='#' onClick={ () => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}>
+          <FaHome />
+          </a>
+      <a href='#about'onClick={ () => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}>
+        <FaRegUserCircle />
+        </a>
+       <a href='#experience' onClick={ () => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}>
+        <RiBook3Line />
+        </a>
+      <a href='#services'onClick={ () => setActiveNav('#services')}
+        className={activeNav === '#services' ? 'active' : ''}>
+        <RiServiceLine />
+        </a>
+      <a href='#contact'onClick={ () => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}>
+        <BiMessageDetail />
+        </a>
 
     </nav>
   )
