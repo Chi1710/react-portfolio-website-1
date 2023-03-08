aba809ebfaacc50f943ee763b46693c21d539b49
Chi <chiyantsestudio@gmail.com>
2023-03-07 19:23:14 -0500

    deleted unuse components in App, added a photo of my recent headshot in assets to use in header. Change color theme in index.css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import "./portfolio.css"
+// import IMG1 from '../../assets/portfolio1.jpg';
+// import IMG2 from '../../assets/portfolio2.jpg';
+// import IMG3 from '../../assets/portfolio3.jpg';
+// import IMG4 from '../../assets/portfolio4.jpg';
+// import IMG5 from '../../assets/portfolio5.jpg';
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container container__portfolio">
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <h3>This is a portfolio item title</h3>
+            <a href="https://github.com" className='btn' target='_blank' rel='noreferrer'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'target='_blank' rel='noreferrer'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
-export default Portfolio
+export default Portfolio;
