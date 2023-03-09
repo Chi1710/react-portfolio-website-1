86d21670a690cbf909a131571147539496523328
Chi <chiyantsestudio@gmail.com>
2023-03-08 20:29:25 -0500

    added the current link to the Socials icon, finish the contact page with my email and message box

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import "./contact.css";
+import {MdAlternateEmail} from 'react-icons/md'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contect Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdAlternateEmail className='contact__icon'/>
+            <h4>Email</h4>
+            <h5>chiyantsestudio@gmail.com</h5>
+            <a href='mailto:chiyantsestudio@gmail.com' alt='Chiyan Tse email'>Send a Message</a>
+          </article>
+          
+        </div>
+        {/* END OF CONTACCT OPTION */}
+        <form action=''>
+          <input type="text" name="name" placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
       </section>
   )
 }
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import {GiFlowers} from "react-icons/gi"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target="_blank"rel="noopener noreferrer"><BsLinkedin /></a>
-        <a href='https://github.com' target="_blank" rel="noopener noreferrer"><TfiGithub /></a>
+        <a href='https://www.linkedin.com/in/chiyantse/' target="_blank"rel="noopener noreferrer"><BsLinkedin /></a>
+        <a href='https://github.com/Chi1710' target="_blank" rel="noopener noreferrer"><TfiGithub /></a>
         <a href='https://www.chiyantse.com' target="_blank" rel="noopener noreferrer"><GiFlowers /></a>
     </div>
   )
